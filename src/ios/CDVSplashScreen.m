07cc89ddbcba3eeee2f5d162e59dd7b546b2563b
Gregor Ollmann <gregor@iwa-tech.com>
2015-09-07 13:36:44 +0200

    Updated Splashscreen logo size for iPad

--- src/ios/CDVSplashScreen.m
+++ src/ios/CDVSplashScreen.m
@@ -324,7 +324,13 @@
                 
             } else {
                 _labelView.frame = CGRectMake(0, screenSize.height/2 - 40 - (screenSize.width/(2.5*(img.size.width/img.size.height))), screenSize.width, 30);
-                _imageView.frame=CGRectMake(screenSize.width/2.5, 0, screenSize.width/1.25, screenSize.height);
+                if(screenSize.width >= 768) {
+                    // iPad
+                    _imageView.frame=CGRectMake(screenSize.width/5, 0, screenSize.width/2.5, screenSize.height);
+                } else {
+                    // iPhone
+                    _imageView.frame=CGRectMake(screenSize.width/2.5, 0, screenSize.width/1.25, screenSize.height);
+                }
             }
         }
     } else {
